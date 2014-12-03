041d05ef4ccd6aebd7c4b4431252a490a2788efa
Mark Mikofski <bwana.marko@yahoo.com>
2014-12-03 04:11:19 +0000

    setup.py edited online with Bitbucket
    * require Pygments 2.0.1 and Sphinx 1.2
    * fixes #31

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst', 'r') as f_readme:
 
 version = '0.2.6'
 
-requires = ['Sphinx>=0.6']
+requires = ['Sphinx>=1.2', 'Pygments>=2.0.1']
 
 setup(
     name='sphinxcontrib-matlabdomain',
