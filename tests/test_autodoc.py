49a1f6d9544b856aa906deb8d52460de99ac19ad
Jørgen Cederberg <jorgen@cederberg.be>
2018-01-03 06:38:29 +0100

    Add test of auto-doc through Sphinx app. Inspired by Sphinx tests

--- tests/test_autodoc.py
+++ tests/test_autodoc.py
@@ -4,16 +4,38 @@ import sphinx
 import sphinxcontrib.matlab as mat
 from sphinx.testing.fixtures import test_params, make_app
 from sphinx.testing.path import path
-import sys
+from sphinx.testing.util import assert_node
+from sphinx import addnodes
 import os
 
+@pytest.fixture(scope='session')
+def rootdir():
+    return path(os.path.dirname(__file__)).abspath()
 
 
-def test_setup(make_app):
-    rootdir = path(os.path.dirname(__file__))
-    srcdir= rootdir / 'test_docs'
+def test_setup(rootdir, make_app):
+    srcdir = rootdir / 'test_docs'
     app = make_app('dummy', srcdir=srcdir)
-    mat.setup(app)
+    app.builder.build_all()
+
+    def assert_refnode(node, module_name, class_name, target, reftype=None,
+                       domain='mat'):
+        attributes = {
+            'refdomain': domain,
+            'reftarget': target,
+        }
+        if reftype is not None:
+            attributes['reftype'] = reftype
+        if module_name is not False:
+            attributes['mat:module'] = module_name
+        if class_name is not False:
+            attributes['mat:class'] = class_name
+        assert_node(node, **attributes)
+
+    doctree = app.env.get_doctree('index')
+    refnodes = list(doctree.traverse(addnodes.pending_xref))
+
+    assert_refnode(refnodes[0],  u'test_data', None, u'test_data', u'mod')
 
 
 if __name__ == '__main__':
