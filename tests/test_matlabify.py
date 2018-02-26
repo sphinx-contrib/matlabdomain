ea3fb1afd0fbed15e79aa1878f6a76e22928916f
Jørgen Cederberg <jorgen@cederberg.be>
2018-02-26 22:21:40 +0100

    Import unicode literals in all test files.

--- tests/test_matlabify.py
+++ tests/test_matlabify.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-from __future__ import print_function
+from __future__ import print_function, unicode_literals
 from sphinxcontrib import mat_documenters as doc
 import os
 import sys
--- tests/test_parse_mfile.py
+++ tests/test_parse_mfile.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import print_function
+from __future__ import print_function, unicode_literals
 from sphinxcontrib import mat_types
 import os
 import pytest
