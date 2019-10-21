7fd8e951616a6d9a17c6900b51b1fbaef247b8bf
Jørgen Cederberg <jorgen@cederberg.be>
2019-10-21 16:32:40 +0200

    Use index as master file

--- tests/roots/test_autodoc/conf.py
+++ tests/roots/test_autodoc/conf.py
@@ -1,11 +1,8 @@
 import os
 
 matlab_src_dir = os.path.abspath('.')
-
 extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.matlab']
 primary_domain = 'mat'
-
-# The suffix of source filenames.
+master_doc = 'index'
 source_suffix = '.rst'
-
 nitpicky = True
