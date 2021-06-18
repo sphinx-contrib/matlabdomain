5671dd0d0479bf02864e46f5b4d6646b177152dd
Jørgen Cederberg <jorgen@cederberg.be>
2021-06-18 10:47:17 +0200

    Revert to use_scm_version = True

--- setup.py
+++ setup.py
@@ -9,10 +9,7 @@ requires = ['Sphinx>=4.0.0', 'Pygments>=2.0.1', 'future>=0.16.0']
 
 setup(
     name='sphinxcontrib-matlabdomain',
-    use_scm_version={
-        'version_scheme': 'guess-next-dev',
-        'local_scheme': 'no-local-version'
-    },
+    use_scm_version=True,
     setup_requires=['setuptools_scm<6.0.0'],
     url='https://github.com/sphinx-contrib/matlabdomain',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-matlabdomain',
