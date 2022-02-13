7ecc8f60bd743bb01e397226c01e7f569457851f
Jørgen Cederberg <jorgen@cederberg.be>
2022-02-13 08:33:50 +0100

    Fix test
    Test failed with the newly introduced "long property string"

--- tests/test_data/ClassAbstract.m
+++ tests/test_data/ClassAbstract.m
@@ -8,7 +8,6 @@ classdef (Abstract = true, Sealed) ClassAbstract < ClassInheritHandle & ClassExa
         version = '0.1.1-beta' % version
     end
     properties (SetAccess = private, GetAccess = private)
-        %: one day this will work
         y % y variable
     end
     methods
