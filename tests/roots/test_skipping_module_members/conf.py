8769663e4e61c42da5de4e0b78e63cea0e94bb2b
Jørgen Cederberg <jorgen@cederberg.be>
2023-02-10 16:46:56 +0100

    Add a test project for skipping module members
    The project `tests/roots/test_skipping_module_members` demonstrates how
    to use the `'autodoc-skip-member'´ feature.

---
+++ tests/roots/test_skipping_module_members/conf.py
@@ -0,0 +1,34 @@
+import os
+
+project = 'Skipping Members'
+copyright = '2023, Jørgen Cederberg'
+author = 'Jørgen Cederberg'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+matlab_src_dir = os.path.abspath('.')
+
+extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.matlab']
+primary_domain = 'mat'
+master_doc = 'index'
+
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+def skip_second(app, what, name, obj, skip, options):
+    print(f"{app}, {what}, {name}, {obj}, {skip}, {options}")
+    return name == "second"
+
+def setup(app):
+    app.connect('autodoc-skip-member', skip_second)
---
+++ tests/test_skipping_module_members.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+    test_autodoc
+    ~~~~~~~~~~~~
+
+    Test the autodoc extension.
+
+    :copyright: Copyright 2007-2018 by the Sphinx team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+import pickle
+import os
+import sys
+
+import pytest
+
+from sphinx import addnodes
+from sphinx.testing.fixtures import make_app, test_params   # noqa: F811;
+from sphinx.testing.path import path
+
+
+@pytest.fixture(scope='module')
+def rootdir():
+    return path(os.path.dirname(__file__)).abspath()
+
+
+@pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python3.6 or higher")
+def test_setup(make_app, rootdir):
+    srcdir = rootdir / 'roots' / 'test_skipping_module_members'
+    app = make_app(srcdir=srcdir)
+    app.builder.build_all()
+
+    content = pickle.loads((app.doctreedir / 'index.doctree').read_bytes())
+    content_text = content.astext()
+    assert 'The first function' in content_text
+    assert 'The second function' not in content_text
+
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
