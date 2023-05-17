a5c1eaa710aaa21354d6afd897e4fc97a70834bf
Jørgen Cederberg <jorgen@cederberg.be>
2023-05-17 12:32:59 +0200

    Fix issue if `matlab_src_dir` is not set. (#187)

---
+++ tests/roots/test_no_matlab_src_dir/conf.py
@@ -0,0 +1,7 @@
+import os
+
+extensions = ["sphinx.ext.autodoc", "sphinxcontrib.matlab"]
+project = "test_no_matlab_src_dir"
+master_doc = "index"
+source_suffix = ".rst"
+nitpicky = True
---
+++ tests/test_no_matlab_src_dir.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+    test_autodoc
+    ~~~~~~~~~~~~
+
+    Test the autodoc extension.
+
+    :copyright: Copyright 2007-2018 by the Sphinx team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+import pickle
+import os
+import sys
+
+import pytest
+
+from sphinx import addnodes
+from sphinx.testing.fixtures import make_app, test_params  # noqa: F811;
+from sphinx.testing.path import path
+
+
+@pytest.fixture(scope="module")
+def rootdir():
+    return path(os.path.dirname(__file__)).abspath()
+
+
+@pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python3.6 or higher")
+def test_make(make_app, rootdir):
+    srcdir = rootdir / "roots" / "test_no_matlab_src_dir"
+    app = make_app(srcdir=srcdir)
+    app.builder.build_all()
+
+    content = pickle.loads((app.doctreedir / "index.doctree").read_bytes())
+    assert len(content) == 1
+    assert content[0].astext() == "Description\n\nEmpty."
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
