f4f88bf9037e40fccbeb09dd0c76517f884a1387
Jørgen Cederberg <jorgen@cederberg.be>
2023-08-20 22:34:14 +0200

    CI: Try-fix path issue

--- tests/helper.py
+++ tests/helper.py
@@ -11,16 +11,16 @@
 from sphinx import version_info as sphinx_version_info
 import os.path
 
-if sphinx_version_info[0] > 6:
+if sphinx_version_info[0] >= 7 and sphinx_version_info[1] >= 2:
     # from sphinx.testing.path was deprecated in version 7.2
     # https://www.sphinx-doc.org/en/master/extdev/deprecated.html
-    from pathlib import Path as path
+    from pathlib import Path
 
     def rootdir(the_file):
-        return path(os.path.dirname(__file__)).absolute()
+        return Path(os.path.dirname(__file__)).absolute()
 
 else:
-    from sphinx.testing.path import path
+    from sphinx.testing.path import path as sphinx_path
 
     def rootdir(the_file):
-        return path(os.path.dirname(__file__)).abspath()
+        return sphinx_path(os.path.dirname(__file__)).abspath()
