sphinx-contrib/matlabdomain

commit 64de0f6a2e5c79260701b125f859e319322e02d1

Working on #204
- Detect if we are about overwrite an entry in the `entities_table`.
- Get `mod` when needed.

sphinxcontrib/mat_documenters.py
@@ -140,7 +140,10 @@ class MatlabDocumenter(PyDocumenter):
                 self.object = self.get_attr(obj, self.objpath[1])
             else:
                 lookup_name = self.fullname.lstrip(".")
-                self.object = entities_table[lookup_name]
+                mod = entities_table[lookup_name]
+                if isinstance(mod, dict):
+                    mod = mod["mod"]
+                self.object = mod
             return True
         # this used to only catch SyntaxError, ImportError and AttributeError,
         # but importing modules with side effects can raise all kinds of errors

sphinxcontrib/mat_types.py
@@ -284,7 +284,15 @@ def analyze(app):
     for name, entity in entities_table.items():
         short_name = shortest_name(name)
         if short_name != name:
-            short_names[short_name] = entity
+            if short_name in entities_table:
+                # Special Case - ClassName/ClassName.m
+                existing_entity = entities_table[short_name]
+                short_names[short_name] = {
+                    entity.ref_role(): entity,
+                    existing_entity.ref_role(): existing_entity,
+                }
+            else:
+                short_names[short_name] = entity
             entities_name_map[short_name] = short_name
 
     entities_table.update(short_names)
@@ -401,6 +409,8 @@ class MatObject(object):
             if package.startswith("_") or package.startswith("."):
                 return None
             mod = entities_table.get(package)
+            if isinstance(mod, dict):
+                mod = mod["mod"]
             if mod:
                 logger.debug(
                     "[sphinxcontrib-matlabdomain] Module %s already loaded.", package
@@ -1752,6 +1762,8 @@ class MatModuleAnalyzer(object):
                 raise entry
             return entry
         mod = entities_table[modname]
+        if isinstance(mod, dict):
+            mod = mod["mod"]
         if mod:
             obj = cls.for_folder(mod.path, modname)
         else:
@@ -1789,6 +1801,8 @@ class MatModuleAnalyzer(object):
         attr_visitor_tagorder = {}
         tagnumber = 0
         mod = entities_table[self.modname]
+        if isinstance(mod, dict):
+            mod = mod["mod"]
 
         # walk package tree
         for k, v in mod.safe_getmembers():

tests/roots/test_class_subfolder/Makefile
@@ -1,0 +1,20 @@
+# Minimal makefile for Sphinx documentation
+#
+
+# You can set these variables from the command line, and also
+# from the environment for the first two.
+SPHINXOPTS    ?=
+SPHINXBUILD   ?= sphinx-build
+SOURCEDIR     = .
+BUILDDIR      = _build
+
+# Put it first so that "make" without argument is like "make help".
+help:
+	@$(SPHINXBUILD) -M help "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)
+
+.PHONY: help Makefile
+
+# Catch-all target: route all unknown targets to Sphinx using the new
+# "make mode" option.  $(O) is meant as a shortcut for $(SPHINXOPTS).
+%: Makefile
+	@$(SPHINXBUILD) -M $@ "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)

tests/roots/test_class_subfolder/Myclass/Myclass.m
@@ -1,0 +1,13 @@
+classdef Myclass
+    % mytest
+
+    properties
+        % prop
+        prop;
+    end
+    methods
+        function f(self)
+            % function
+        end
+    end
+end

tests/roots/test_class_subfolder/conf.py
@@ -1,0 +1,17 @@
+import sys
+import os
+
+# General information about the project.
+project = "test_docs_automodule"
+copyright = "2023, Jørgen Cederberg"
+author = "Jørgen Cederberg"
+
+# Setup extension
+sys.path.insert(0, os.path.abspath(os.path.join("..")))
+matlab_src_dir = os.path.abspath(".")
+extensions = ["sphinx.ext.autodoc", "sphinxcontrib.matlab"]
+primary_domain = "mat"
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+html_theme = "default"

tests/roots/test_class_subfolder/make.bat
@@ -1,0 +1,35 @@
+@ECHO OFF
+
+pushd %~dp0
+
+REM Command file for Sphinx documentation
+
+if "%SPHINXBUILD%" == "" (
+	set SPHINXBUILD=sphinx-build
+)
+set SOURCEDIR=.
+set BUILDDIR=_build
+
+%SPHINXBUILD% >NUL 2>NUL
+if errorlevel 9009 (
+	echo.
+	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
+	echo.installed, then set the SPHINXBUILD environment variable to point
+	echo.to the full path of the 'sphinx-build' executable. Alternatively you
+	echo.may add the Sphinx directory to PATH.
+	echo.
+	echo.If you don't have Sphinx installed, grab it from
+	echo.https://www.sphinx-doc.org/
+	exit /b 1
+)
+
+if "%1" == "" goto help
+
+%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+goto end
+
+:help
+%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+
+:end
+popd

tests/test_class_subfolder.py
@@ -1,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+    test_class_subfolder
+    ~~~~~~~~~~~~
+
+    Test the autodoc extension.
+
+    :copyright: Copyright 2007-2018 by the Sphinx team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+import pickle
+import sys
+import pytest
+import helper
+from sphinx import addnodes
+from sphinx.testing.fixtures import make_app, test_params  # noqa: F811;
+
+
+@pytest.fixture(scope="module")
+def rootdir():
+    return helper.rootdir(__file__)
+
+
+@pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python3.6 or higher")
+def test_target(make_app, rootdir):
+    srcdir = rootdir / "roots" / "test_class_subfolder"
+    app = make_app(srcdir=srcdir)
+    app.builder.build_all()
+
+    content = pickle.loads((app.doctreedir / "index.doctree").read_bytes())
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])

commit 3e262bd240ccd86f87bcb1b34a25f05e37e23e6b

Merging updates to combine fixes to nested classes and modules broken by short_names (#238)
* Added catch to limit short_names shortening to modules

Fixes a problem introduced by 64de0f6 (see #235)

* Fixed a problem preventing repeated directory names in subdirs

* Updated test_module_class_names readme to reflect expanded test

* Fixed typo in comment

* Updated comment on when short_names are prevented

* Changed to double-quote string to match style convention

AUTHORS
@@ -15,3 +15,4 @@ Contributors
 * ptitrex
 * Isaac Lenton (ilent2)
 * Ray Zimmerman
+* Marshall J. Styczinski

CITATION.bib
@@ -15,12 +15,13 @@
                Hugo Leblanc and
                Marc-Henri Bleu-Laine and
                Nikolaas N. Oosterhof and
-               ptitrex},
-  title     = {sphinx-contrib/matlabdomain: 0.21.2},
+               ptitrex and
+               Marshall J. Styczinski},
+  title     = {sphinx-contrib/matlabdomain: 0.21.3},
   month     = jan,
   year      = 2024,
   publisher = {Zenodo},
-  version   = {0.21.2},
+  version   = {0.21.3},
   doi       = {10.5281/zenodo.7746009},
   url       = {https://github.com/sphinx-contrib/matlabdomain}
 }

sphinxcontrib/mat_documenters.py
@@ -1225,6 +1225,8 @@ class MatClassDocumenter(MatModuleLevelDocumenter):
             membername
             for (membername, member) in filtered_members
             if not isinstance(member, MatMethod) and not isinstance(member, MatProperty)
+            # exclude parent modules with names matching members (as in Myclass.Myclass)
+            and not (hasattr(member, "module") and member.name == member.module)
         ]
         # create list of members that are not properties
         non_properties = [

sphinxcontrib/mat_types.py
@@ -290,9 +290,12 @@ def analyze(app):
     #
     # NOTE: Does not yet work with class folders
     short_names = {}
+    long_names = entities_table.keys()
     for name, entity in entities_table.items():
         short_name = shortest_name(name)
-        if short_name != name:
+        if short_name != name and not (short_name in long_names and name in long_names):
+            # Only handle the below special case when overwriting entries in entities_table will not
+            # introduce conflicts
             if short_name in entities_table:
                 # Special Case - ClassName/ClassName.m
                 existing_entity = entities_table[short_name]
@@ -1769,8 +1772,10 @@ class MatModuleAnalyzer(object):
                 raise entry
             return entry
         mod = try_get_module_entity_or_default(modname)
-        if mod:
+        if isinstance(mod, MatModule):
             obj = cls.for_folder(mod.path, modname)
+        elif isinstance(mod, MatClass):
+            obj = cls.for_folder(mod.module, modname)
         else:
             err = MatcodeError("error importing %r" % modname)
             cls.cache["module", modname] = err

tests/roots/test_module_class_names/Myclass/MyOtherClass/MyOtherClass.m
@@ -1,0 +1,13 @@
+classdef MyOtherClass
+    % myothertest
+
+    properties
+        % prop
+        otherprop;
+    end
+    methods
+        function otherf(self)
+            % function
+        end
+    end
+end

tests/test_module_class_names.py
@@ -31,6 +31,7 @@ def test_index(make_app, rootdir):
     assert (
         content.astext()
         == "Myclass\n\n\n\nclass Myclass\n\nMyclass\n\nSee Also: YourClass\n\nConstructor Summary\n\n\n\n\n\nMyclass()\n\nThe Myclass constructor\n\nProperty Summary\n\n\n\n\n\nprop\n\nThe property\n\nMethod Summary\n\n\n\n\n\nadd(value)\n\nAdd the value\n\nYourClass\n\n\n\nclass YourClass\n\nYourClass\n\nSee Also: Myclass\n\nConstructor Summary\n\n\n\n\n\nYourClass()\n\nThe YourClass constructor\n\nProperty Summary\n\n\n\n\n\nprop\n\nThe property\n\nMethod Summary\n\n\n\n\n\nadd(value)\n\nAdd the value"
+        + "\n\n\n\n\n\n\n\nclass MyOtherClass\n\nmyothertest\n\nProperty Summary\n\n\n\n\n\notherprop\n\nprop\n\nMethod Summary\n\n\n\n\n\notherf()\n\nfunction"
     )
 
 
@@ -45,6 +46,7 @@ def test_index_auto_link_all(make_app, rootdir):
     assert (
         content.astext()
         == "Myclass\n\n\n\nclass Myclass\n\nMyclass\n\nSee Also: YourClass\n\nConstructor Summary\n\n\n\n\n\nMyclass()\n\nThe Myclass() constructor\n\nProperty Summary\n\n\n\n\n\nprop\n\nThe property\n\nMethod Summary\n\n\n\n\n\nadd(value)\n\nAdd the value\n\nYourClass\n\n\n\nclass YourClass\n\nYourClass\n\nSee Also: Myclass\n\nConstructor Summary\n\n\n\n\n\nYourClass()\n\nThe YourClass() constructor\n\nProperty Summary\n\n\n\n\n\nprop\n\nThe property\n\nMethod Summary\n\n\n\n\n\nadd(value)\n\nAdd the value"
+        + "\n\n\n\n\n\n\n\nclass MyOtherClass\n\nmyothertest\n\nProperty Summary\n\n\n\n\n\notherprop\n\nprop\n\nMethod Summary\n\n\n\n\n\notherf()\n\nfunction"
     )
 
 