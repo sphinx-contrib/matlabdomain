9c41c187ea81641a559901d4f2ca7abd0fbb951d
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2024-07-17 11:09:21 +0200

    remove vestigial file

--- sphinxcontrib/generate_example_class.py
+++
@@ -1,8 +0,0 @@
-from textmate_grammar.parsers.matlab import MatlabParser
-
-rpath = "../../../syscop/software/nosnoc/+nosnoc/Options.m"
-
-if __name__ == "__main__":
-    parser = MatlabParser()
-    parsed = parser.parse_file(rpath)
-    parsed.print()
