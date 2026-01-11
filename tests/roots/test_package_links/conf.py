dad916c773a548bf93ff5a6425ac4eb47c8efa51
Remi Gau <remi_gau@hotmail.com>
2026-01-11 18:37:20 +0100

    [MAINT] test build all test docs in strict mode

--- tests/roots/test_package_links/conf.py
+++ tests/roots/test_package_links/conf.py
@@ -6,3 +6,5 @@ matlab_src_dir = os.path.abspath(".")
 matlab_keep_package_prefix = True
 primary_domain = "mat"
 nitpicky = True
+# ignore warning about inheriting from unknown 'handle' class
+nitpick_ignore = [("mat:class", "handle")]
