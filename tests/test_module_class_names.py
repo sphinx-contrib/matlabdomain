34b04224f0db34aa7af94b5e4e8d1d66fcb07295
Remi Gau <remi_gau@hotmail.com>
2026-01-11 10:12:28 +0100

    [WIP] use fixtures for tests (#330)
    * [REF] centralize tests for each module
    
    * rm run test modules as script
    
    * use fixtures
    
    * use app fixture
    
    * test_mate_types from master
    
    * refactor
    
    * ref
    
    * ref
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * rm helper test function
    
    * refactor import
    
    * parametrize test
    
    * make test more readable
    
    * parametrize
    
    * refactor
    
    * parametrize
    
    * refactor
    
    * style
    
    * fix
    
    * doc
    
    * fix

--- tests/test_module_class_names.py
+++ tests/test_module_class_names.py
@@ -9,48 +9,61 @@ Test the autodoc extension.
 
 import pickle
 
-import helper
 import pytest
 
 
-@pytest.fixture(scope="module")
-def rootdir():
-    return helper.rootdir(__file__)
+@pytest.fixture
+def srcdir(rootdir):
+    return rootdir / "roots" / "test_module_class_names"
 
 
-def test_index(make_app, rootdir):
-    srcdir = rootdir / "roots" / "test_module_class_names"
-    app = make_app(srcdir=srcdir)
-    app.builder.build_all()
+@pytest.fixture
+def confdict(matlab_auto_link):
+    return {"matlab_auto_link": matlab_auto_link}
 
+
+@pytest.mark.parametrize("matlab_auto_link", ["basic", "all"])
+def test_module_class_names(app, confdict):
     content = pickle.loads((app.doctreedir / "index.doctree").read_bytes())
-    assert (
-        content.astext()
-        == "Myclass\n\n\n\nclass Myclass\n\nMyclass\n\nSee Also: YourClass\n\nConstructor Summary\n\n\n\n\n\nMyclass()\n\nThe Myclass constructor\n\nProperty Summary\n\n\n\n\n\nprop\n\nThe property\n\nMethod Summary\n\n\n\n\n\nadd(value)\n\nAdd the value\n\nYourClass\n\n\n\nclass YourClass\n\nYourClass\n\nSee Also: Myclass\n\nConstructor Summary\n\n\n\n\n\nYourClass()\n\nThe YourClass constructor\n\nProperty Summary\n\n\n\n\n\nprop\n\nThe property\n\nMethod Summary\n\n\n\n\n\nadd(value)\n\nAdd the value"
-        + "\n\n\n\n\n\n\n\nclass MyOtherClass\n\nmyothertest\n\nProperty Summary\n\n\n\n\n\notherprop\n\nprop\n\nMethod Summary\n\n\n\n\n\notherf()\n\nfunction"
+
+    expected_content = (
+        "Myclass\n\n\n\n"
+        "class Myclass\n\n"
+        "Myclass\n\n"
+        "See Also: YourClass\n\n"
+        "Constructor Summary\n\n\n\n\n\n"
+        "Myclass()\n\n"
+        "The Myclass{} constructor\n\n"
+        "Property Summary\n\n\n\n\n\n"
+        "prop\n\n"
+        "The property\n\n"
+        "Method Summary\n\n\n\n\n\n"
+        "add(value)\n\n"
+        "Add the value\n\n"
+        "YourClass\n\n\n\n"
+        "class YourClass\n\n"
+        "YourClass\n\n"
+        "See Also: Myclass\n\n"
+        "Constructor Summary\n\n\n\n\n\n"
+        "YourClass()\n\n"
+        "The YourClass{} constructor\n\n"
+        "Property Summary\n\n\n\n\n\n"
+        "prop\n\n"
+        "The property\n\n"
+        "Method Summary\n\n\n\n\n\n"
+        "add(value)\n\n"
+        "Add the value\n\n\n\n\n\n\n\n"
+        "class MyOtherClass\n\n"
+        "myothertest\n\n"
+        "Property Summary\n\n\n\n\n\n"
+        "otherprop\n\n"
+        "prop\n\n"
+        "Method Summary\n\n\n\n\n\notherf()\n\n"
+        "function"
     )
 
+    if confdict["matlab_auto_link"] == "basic":
+        assert content.astext() == expected_content.format("", "")
 
-def test_index_auto_link_all(make_app, rootdir):
-    srcdir = rootdir / "roots" / "test_module_class_names"
-    confdict = {"matlab_auto_link": "all"}
-    app = make_app(srcdir=srcdir, confoverrides=confdict)
-    app.builder.build_all()
-
-    content = pickle.loads((app.doctreedir / "index.doctree").read_bytes())
-    assert content.astext() == (
-        "Myclass\n\n\n\nclass Myclass\n\nMyclass\n\n"
-        "See Also: YourClass\n\nConstructor Summary\n\n\n\n\n\n"
-        "Myclass()\n\nThe Myclass() constructor\n\n"
-        "Property Summary\n\n\n\n\n\nprop\n\nThe property\n\n"
-        "Method Summary\n\n\n\n\n\nadd(value)\n\nAdd the value\n\n"
-        "YourClass\n\n\n\nclass YourClass\n\nYourClass\n\n"
-        "See Also: Myclass\n\nConstructor Summary\n\n\n\n\n\n"
-        "YourClass()\n\nThe YourClass() constructor\n\n"
-        "Property Summary\n\n\n\n\n\nprop\n\nThe property\n\n"
-        "Method Summary\n\n\n\n\n\nadd(value)\n\n"
-        "Add the value\n\n\n\n\n\n\n\nclass MyOtherClass"
-        "\n\nmyothertest\n\nProperty Summary\n\n\n\n\n\n"
-        "otherprop\n\nprop\n\nMethod Summary\n\n\n\n\n\n"
-        "otherf()\n\nfunction"
-    )
+    elif confdict["matlab_auto_link"] == "all":
+        assert content.astext() == expected_content.format("()", "()")
